85c374037f495e2b20844d8ccb10a919bd05a9b4
mellolucas <dev@lufom.com>
2022-06-10 12:25:08 -0300

    feat(bashrcd): sourcing aliases from zshrcd

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,30 +1,7 @@
-# main ----------------------------------------
-alias cls='clear'
-alias ll="ls -pilhA --color=auto"
-alias ncat="cat --number"
-
-# git ----------------------------------------
-alias g="git"
-alias ga="git add"
-alias gc="git commit -m"
-alias gs="git status"
-alias gd="git diff"
-alias gf="git fetch"
-alias gm="git merge"
-alias gr="git rebase"
-alias gp="git push"
-alias gu="git unstage"
-alias gg="git graph"
-alias ggg="git graphgpg"
-alias gco="git checkout"
-alias gcs="git commit -S -m"
-alias gpr="hub pull-request"
-
-# Containers ----------------------------------------
-alias k8="kubectl"
-
-# Misc ----------------------------------------
-alias lol="base64 </dev/urandom | lolcat"
-alias matrix="cmatrix -b"
-alias zen="while :; do bonsai -l -b 2 -c oO0 -t 0.5; sleep 10; done"
-
+if [ -d $HOME/.zshrc.d ]; then
+  for file in $HOME/.zshrc.d/*alias*; do
+    if [ -f "$file" ]; then
+      . "$file"
+    fi
+  done
+fi
---
+++ .bashrc.d/completions.sh
@@ -0,0 +1,5 @@
+# pip
+
+
+# nerdctl
+[[ $(command -v nerdctl) ]] && source <(nerdctl completion bash)
