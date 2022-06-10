b2927e616c344e32379bda1f64b319a3a157d9ad
mellolucas <dev@lufom.com>
2022-06-10 12:16:02 -0300

    feat(templates): update bash script template

--- Templates/tpl_sh.sh
+++ Templates/tpl_sh.sh
@@ -18,10 +18,11 @@ clear_line="\e[K"
 cat_section() {
   input=${@}
   input_length=${#input}
-  let "sep_n=76-${input_length}"
+  let "tput_cols=$(tput cols)-2"
+  let "sep_n=${tput_cols}-2-${input_length}"
   let "lsep_n=$sep_n/2"
   let "rsep_n=$sep_n/2+$sep_n%2"
-  sep=$(eval printf -- '-%.0s' {1..78})
+  sep=$(eval printf -- '-%.0s' {1..${tput_cols}})
   lsep=$(eval printf -- '-%.0s' {1..${lsep_n}})
   rsep=$(eval printf -- '-%.0s' {1..${rsep_n}})
   echo -e "${aes_draw}#${sep}#${clear_line}"
@@ -31,7 +32,19 @@ cat_section() {
   echo -e "${aes_draw}#${sep}#${clear_line}"
 }
 cat_text() {
-  echo -e "${aes_text}$1${clear_line}${aes_reset}"
+  echo -e "${aes_text}$1${clear_line}"
+  sleep 1
+}
+cat_draw() {
+  echo -e "\e[5m$1${aes_text}${clear_line}"
+}
+cat_step() {
+  echo -e "${aes_text}$1\e[5m ... ${aes_text}${clear_line}"
+  sleep 1
+}
+cat_link() {
+  echo -e "  \e[4;38;5;159m$1\e[24m${aes_text}${clear_line}"
+  sleep 1
 }
 color_line() {
   echo -en "${aes_text}${clear_line}"
@@ -44,35 +57,29 @@ screen_reset() {
 }
 
 # Start ------------------------------------------------------------------------
+echo
+echo
+echo
+echo
 screen_paint 
 cat_section "Starting ${script_title} Script"
 color_line
+echo
 
 # Code -----
-whoami
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
-echo aaa
+cat_text "This script requires super user privileges (sudo)!"
+cat_text "You will soon be asked to enter the password for $USER ."
+echo
+tput bel
+read -p "Press [Enter] to continue..."
+echo
+cat_step "Starting script"
+echo
 
 # End --------------------------------------------------------------------------
+echo
 cat_section "/Finished ${script_title} Script"
 screen_reset
 echo -e "\e[50B"
+echo
 echo
